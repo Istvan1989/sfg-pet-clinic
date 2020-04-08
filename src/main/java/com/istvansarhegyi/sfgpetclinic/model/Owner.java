ba694c15af80c622683e51fe4c39186d0396bc45
Istvan1989 <istvan.sarhegyi@gmail.com>
2020-04-08 16:08:50 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/com/istvansarhegyi/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.istvansarhegyi.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/istvansarhegyi/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.istvansarhegyi.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
