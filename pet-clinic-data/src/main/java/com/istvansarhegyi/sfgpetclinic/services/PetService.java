4d4671bf8cdd14e7d64b1979c4d5a9d488cef5a9
Istvan1989 <istvan.sarhegyi@gmail.com>
2020-04-11 15:15:19 +0200

    refactor service interfaces. Closes #11

--- pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.istvansarhegyi.sfgpetclinic.services;
 
 import com.istvansarhegyi.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
