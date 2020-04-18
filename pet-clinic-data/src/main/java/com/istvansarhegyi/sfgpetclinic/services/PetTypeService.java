6569651c364b33e5d1d866ee202764efc6942ed4
Istvan1989 <istvan.sarhegyi@gmail.com>
2020-04-18 15:56:30 +0200

    Created PetType Map Service Impl closes #29

---
+++ pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.istvansarhegyi.sfgpetclinic.services;
+
+import com.istvansarhegyi.sfgpetclinic.model.PetType;
+
+public interface PetTypeService  extends CrudService<PetType, Long> {
+
+}
