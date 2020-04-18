a488104f0e4b75a340d6926cd9a6ee5000a9e8b7
Istvan1989 <istvan.sarhegyi@gmail.com>
2020-04-18 16:28:44 +0200

    Created Specialty Map Service Impl closes #31

---
+++ pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.istvansarhegyi.sfgpetclinic.services;
+
+import com.istvansarhegyi.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
