57a93470411691df8c8cd90f47115e4300af8645
Istvan1989 <istvan.sarhegyi@gmail.com>
2020-04-20 11:19:30 +0200

    Converted Owners to JPA Entities closes #36

--- pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/istvansarhegyi/sfgpetclinic/model/Pet.java
@@ -1,13 +1,25 @@
 package com.istvansarhegyi.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+@Entity
+@Table(name = "pets")
+public class Pet extends BaseEntity {
 
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birtDate;
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birth_date")
+    private LocalDate birtDate;
 
     public String getName() {
         return name;
